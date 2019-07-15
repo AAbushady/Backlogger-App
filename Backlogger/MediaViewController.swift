e1296069f4560dc4c0abbed2bdd87565e97e770d
Alexander Abushady <aabushady214@gmail.com>
2019-07-15 14:50:16 -0400

    Finished Up
    Completed Core Data functionality. Also set code allowing for use of camera to take photos for the backlog.

--- Backlogger/MediaViewController.swift
+++ Backlogger/MediaViewController.swift
@@ -13,15 +13,30 @@ class MediaViewController: UIViewController, UIImagePickerControllerDelegate, UI
     // These are the outlets that link their corresponding fields in the View Controller.
     @IBOutlet weak var mediaImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var addUpdateButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
     
     // Allows for the ability to either take a new photo or choose an existing one.
     var imagePicker = UIImagePickerController()
+    
+    // This variable helps when choosing whether to use the add or update button and whether or not to add a delete button.
+    var media : Media? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Dictates where image is sent and what to do with it. This code is set within the View Controller.
         imagePicker.delegate = self
+        
+        // Dictates what type of interface the user is going to get in the Media View Controller.
+        if media != nil {
+            mediaImageView.image = UIImage(data: media!.image as! Data)
+            titleTextField.text = media?.title
+            
+            addUpdateButton.setTitle("Update", for: .normal)
+        } else {
+            deleteButton.isHidden = true
+        }
     }
     // Function that completes when the photos button is tapped.
     @IBAction func photosTapped(_ sender: Any) {
@@ -46,22 +61,43 @@ class MediaViewController: UIViewController, UIImagePickerControllerDelegate, UI
     
     // Function that completes when the camera button is tapped.
     @IBAction func cameraTapped(_ sender: Any) {
+        // Directs the imagePicker to the user's photolibrary.
+        imagePicker.sourceType = .camera
+        
+        // Allows for displaying the image picker to the user.
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     // Function that completes when the add button is tapped.
     @IBAction func addTapped(_ sender: Any) {
-        // These constants set up so that we can use the Entities in Core Data.
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let media = Media(context: context)
-        
-        // Assign the title entered by the user to the Entity.
-        media.title = titleTextField.text
-        // Assign the image entered by the user to the Entity.
-        media.image = mediaImageView.image!.pngData()
+        if media != nil {
+            media!.title = titleTextField.text
+            media!.image = mediaImageView.image!.pngData()
+        } else {
+            // These constants set up so that we can use the Entities in Core Data.
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let media = Media(context: context)
+            
+            // Assign the title entered by the user to the Entity.
+            media.title = titleTextField.text
+            // Assign the image entered by the user to the Entity.
+            media.image = mediaImageView.image!.pngData()
+        }
         
         // Send completed Media entity to Core Data.
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         
         navigationController!.popViewController(animated: true)
+    }
+    
+    // This will delete an entity and update Core Data. Afterwards you will be sent back to the main view controler.
+    @IBAction func deleteTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(media!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)        
     }
 }
--- Backlogger/ViewController.swift
+++ Backlogger/ViewController.swift
@@ -9,22 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    // This outlet is for the table view.
     @IBOutlet weak var tableView: UITableView!
     
+    // This array will allow for showing the Core Data in the Table View.
     var mediaArray : [Media] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+        // Assigns the how many cells and what are in the cells in the table view.
         tableView.dataSource = self
         tableView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        // Context allows for interaction with Core Data.
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         do {
+            // This will allow the table view to be updated when Core Data is changed.
             mediaArray = try context.fetch(Media.fetchRequest())
             tableView.reloadData()
         } catch {
@@ -32,10 +34,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    // This function is for setting how many rows will be in the table view based on how many entities are in Core Data.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mediaArray.count
     }
     
+    // Populate each cell with it's respective information.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let media = mediaArray[indexPath.row]
@@ -44,6 +48,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
 
+    // These two functions will take the information from Core Data based on the index of the entity and fill the Media View Controller with the appropriate information.
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let media = mediaArray[indexPath.row]
+        performSegue(withIdentifier: "mediaSegue", sender: media)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! MediaViewController
+        nextVC.media = sender as? Media
+    }
 
 }
 
